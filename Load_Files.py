151600e1625aec0110347f0de30e1777732d3340
ValC42 <128488733+ValC42@users.noreply.github.com>
2023-05-02 15:29:17 +0200

    recherche chemin bon

--- Load_Files.py
+++ Load_Files.py
@@ -9,13 +9,9 @@ import json
 import math
 from queue import PriorityQueue
 
-f_point_debouche = "point_debouche.geojson"
-
-def charger_donnees():
-    arrondi_gps = 12
+def charger_donnees_troncon():
     f_troncons_geojson = "troncon_trame_viaire.geojson"
-    f_chausses_geojson = "chaussees_trotoirs.geojson"
-    f_point_debouche = "point_debouche.geojson"
+    arrondi_gps = 12
    
     with open(f_troncons_geojson,encoding='utf-8') as fichier :
         data = json.load(fichier)
@@ -54,8 +50,11 @@ def charger_donnees():
                 dico_noeuds[tuple_gps][rue]=[]
             if troncon not in dico_noeuds[tuple_gps][rue]:
                 dico_noeuds[tuple_gps][rue].append(troncon)
-    
-                
+    return dico_noeuds, dico_rues
+
+def charger_donnees_chausses(dico_noeuds, dico_rues):                
+    f_chausses_geojson = "chaussees_trotoirs.geojson"
+    arrondi_gps = 12
     with open(f_chausses_geojson,encoding='utf-8') as fichier :
         data = json.load(fichier)
     for segment in data["features"]:
@@ -68,7 +67,6 @@ def charger_donnees():
             co_gps_rue[i][1] = round(co_gps_rue[i][1],arrondi_gps)        
         
         if rue in dico_rues.keys() and troncon in dico_rues[rue].keys():
-            #sens circulation?
             if dico_rues[rue][troncon]["Nom"] == "" or dico_rues[rue][troncon]["Nom"] == "Voie sans denomination":
                 dico_rues[rue][troncon]["Nom"] = proprietes["nomvoie1"]
             if dico_rues[rue][troncon]["Commune"] == "":
@@ -83,6 +81,10 @@ def charger_donnees():
                 dico_rues[rue] = {troncon: {"GPS" : co_gps_rue} }
             else:
                 dico_rues[rue][troncon] = {"GPS" : co_gps_rue}
+            if proprietes["senscirculation"] == "Double":
+                dico_rues[rue][troncon]["Sens_circulation"] = proprietes["senscirculation"]
+            else:
+                dico_rues[rue][troncon]["Sens_circulation"] = None
             dico_rues[rue][troncon]["Nom"] = proprietes["nomvoie1"]
             dico_rues[rue][troncon]["Commune"] = proprietes["commune1"]
             dico_rues[rue][troncon]["Code_postal"] = proprietes["insee1"]
@@ -109,7 +111,9 @@ def charger_donnees():
                 dico_noeuds[tuple_gps][rue]=[]
             if troncon not in dico_noeuds[tuple_gps][rue]:
                 dico_noeuds[tuple_gps][rue].append(troncon)
-        
+    return dico_noeuds, dico_rues
+    
+def correction_dico_noeuds(dico_noeuds):
     a_supprimer = []
     for tuple_gps in dico_noeuds:
         if len(dico_noeuds[tuple_gps]) < 2:
@@ -118,7 +122,9 @@ def charger_donnees():
                     a_supprimer.append(tuple_gps)
     for tuple_gps in a_supprimer:
         dico_noeuds.pop(tuple_gps)
-
+    return dico_noeuds
+    
+def charger_donnees_adj(dico_noeuds):
     rues_adj_gps = {}
     for noeud in dico_noeuds:
         liste_adj = []
@@ -138,12 +144,16 @@ def charger_donnees():
                 for i in liste_adj :
                     if i != (rue,troncon) and i not in rues_adj_gps[(rue,troncon)]:
                         rues_adj_gps[(rue,troncon)].append(i)
-    
+    return rues_adj_gps  
+             
+def charger_donnees_adresses():
+    f_point_debouche = "point_debouche.geojson"
+    arrondi_gps = 12                    
     with open(f_point_debouche,encoding='utf-8') as fichier :
         data = json.load(fichier)
     dico_adresses_num = {}
     dico_adresses_rues = {}
-    l_communes = []
+    dico_adresses_communes = {}
     for adresse in data["features"]:
         proprietes = adresse["properties"]
         numero = proprietes["numero"]
@@ -166,11 +176,15 @@ def charger_donnees():
             dico_adresses_rues[voie][numero] = {}
         if commune not in dico_adresses_rues[voie][numero].keys():
             dico_adresses_rues[voie][numero][commune] = co_gps_adresse
-            
-        if commune not in l_communes:
-            l_communes.append(commune)
-    
-    return rues_adj_gps,dico_rues, dico_adresses_num, dico_adresses_rues, l_communes
+        
+        if commune not in dico_adresses_communes.keys():
+            dico_adresses_communes[commune] = {}
+        if voie not in dico_adresses_communes[commune].keys():
+            dico_adresses_communes[commune][voie] = {}
+        if numero not in dico_adresses_communes[commune][voie].keys():
+            dico_adresses_communes[commune][voie][numero] = co_gps_adresse
+
+    return dico_adresses_num, dico_adresses_rues, dico_adresses_communes
 
 def setup_adjacence_param(rues_adjacentes,dico_rues,largeur_chaussee_m = None ,pente_m = None, revet_chaussee_enl = None ,revet_trot_enl = None ,largeur_trot_max= None):
     """Réduit le dictionnaire d'adjacence des rues total selon les paramètres de l\'utilisateur donnés par l'application
@@ -220,122 +234,159 @@ def give_troncon_nearest_gps(co_gps_user,dico_rues):
         dict: {(FUV,troncon) : [co_gps]}
     """
     d_min = 2e+7 #ici on va comparer les distance carré entre elles (et la distance maximale étant la circonsphérence de la terre)
-    id_rue_troncon = {(0,0) : co_gps_user}
+    id_rue_troncon = (0,0)
     for fuv in dico_rues.keys():
         for troncon in dico_rues[fuv].keys():
             for co_gps in dico_rues[fuv][troncon]['GPS'] :
                 d = dist_lat_lon_deg(co_gps[1],co_gps[0],co_gps_user[1],co_gps_user[0])
                 if d < d_min :
-                    id_rue_troncon = {(fuv,troncon) : co_gps}
+                    id_rue_troncon = (fuv,troncon)
                     d_min = d
 
     return id_rue_troncon
     
-def give_troncon_address(saisie_user, dico_adresses_num, dico_adresses_rues, liste_communes):
-    l_saisie = saisie_user.split(" ")
-    a_suppr = []
-    for i in range(len(l_saisie)):
-        if l_saisie[i] == "" or l_saisie[i] == " ":
-            a_suppr.append(i)
-    a_suppr.reverse()
-    for i in a_suppr:
-        l_saisie.pop(i)
-    print(f"l_saisie : {l_saisie}")        
+def gestion_saisie(saisie_user, l_communes):
+    liste_saisie = saisie_user.split(", ")
+    centre = False
+    commune = None
+    rue = None
+    numero = None
+    if len(liste_saisie) > 1 :
+        commune = liste_saisie.pop(-1)
+        print(f'Commune reconnue : {commune}')
+    else:
+        i = 0
+        while commune == None and i < len(l_communes):
+            if liste_saisie[0].lower() in l_communes[i].lower():
+                commune = l_communes[i] #!!!
+                # commune = liste_saisie[0]
+                print(f'Centre commune reconnu : {commune}')
+                centre = True
+            i += 1
+    if commune == None :
+        print('Commune non-reconnue ou non-renseignée.')
+    if not centre:
+        liste_rue = liste_saisie[0].split(" ")
+        a_suppr = []
+        for i in range(len(liste_rue)):
+            if liste_rue[i] == "" or liste_rue[i] == " ":
+                a_suppr.append(i)
+        a_suppr.reverse()
+        for i in a_suppr:
+            liste_rue.pop(i)
+        # on teste s'il y a un numero
+        try :
+            numero = int(liste_rue[0])
+            liste_rue.pop(0)
+            print(f'Numéro reconnu : {numero}')
+        except :
+            print("Aucun numéro reconnu ou renseigné.")
+        print(f"Rue saisie: {liste_rue}")
+        if len(liste_rue) > 0:
+            rue = ""
+            for i in range(len(liste_rue)):
+                rue += liste_rue[i]
+                if i < len(liste_rue)-1:
+                    rue += " "
+            rue.strip(",")
+            print(f"Rue detectée : {rue}")
+        else:
+            print("Aucune rue saisie")
+    print()
+    return numero, rue, commune, centre
+
+def give_troncon_address(numero, rue, commune, centre, dico_adresses_num, dico_adresses_rues, dico_adresses_communes):
+    nb_prop_max = 6
     liste_adresses = []
-    # on teste s'il y a un numero
-    try :
-        numero = int(l_saisie[0])
-        l_saisie.pop(0)
-        print(numero)
-    except :
-        numero = None
-        print("Aucun numéro detecté")
-    # on essaye de savoir ou en est la saisie
-    # cas ou au moins la rue et peut etre meme et/ou commune commence(nt) à etre indiquée(s)
-    if len(l_saisie) > 0 or numero == None:
-        # cas ou la commune est indiquée et existe dans le GL (=on suppose adresse complete)
-        if l_saisie[-1] in liste_communes:
-            commune = l_saisie.pop(-1)
-            print(commune)
-            rue = None
-            if len(l_saisie) > 0:
-                rue = ""
-                for i in range(len(l_saisie)):
-                    rue += l_saisie[i]
-                    if i < len(l_saisie)-1:
-                        rue += " "
-            print(rue)
+    # on essaye de savoir où en est la saisie
+    
+    # cas où seulement la commune est saisie
+    if centre:
+        print(f"Centre commune : {commune}")
+        liste_adresses.append(commune+" centre")
+    # cas où seul le numero est marqué
+    elif commune == None and rue == None and numero != None:
+        print(f"Numéro uniquement : {numero}")
+        for num in dico_adresses_num.keys():
+            # le numéro existe ou compose un numéro
+            if len(liste_adresses) < nb_prop_max and str(numero).lower() == num.lower():
+                for rue in dico_adresses_num[num].keys():
+                    if len(liste_adresses) < nb_prop_max :
+                        for commune in dico_adresses_num[num][rue].keys():
+                            if len(liste_adresses) < nb_prop_max :
+                                liste_adresses.append(num+" "+rue+", "+commune)
+    # cas où au moins le début de la rue est indiqué mais pas la commune
+    elif commune == None and rue != None:
+        print(f"Rue en partie : {rue}")
+        for voie in dico_adresses_rues.keys():
             # la rue existe
-            if rue in dico_adresses_rues.keys():
-                # le numero n'existe pas mais on propose des alternatives
-                if numero == None or str(numero) not in dico_adresses_rues[rue].keys():
-                    print("le numéro n'existe pas")
-                    for num in dico_adresses_rues[rue].keys():
-                        if len(liste_adresses) < 6 and commune in dico_adresses_rues[rue][num].keys():
-                            liste_adresses.append(num+" "+rue+" "+commune)
-                # l'adresse est parfaite
-                else:
-                    print(numero)
-                    liste_adresses.append(str(numero)+" "+rue+" "+commune)
-            # seule la commune a été saisie ou la rue n'existe pas
-            else: 
-                if rue != None:
-                    print("la rue n'existe pas")
-                    liste_adresses.append("rue inconnue")
-                print("centre commune")
-                liste_adresses.append(commune + " centre")
-                # a develloper
-                
-        #cas ou la commune n'est pas renseignée ou n'est pas dans le Grand Lyon
-        else :
-            commune = None
-            print("La commune n'est pas renseignée ou n'est pas dans le Grand Lyon")
-            rue = None
-            if len(l_saisie) > 0:
-                rue = ""
-                for i in range(len(l_saisie)):
-                    rue += l_saisie[i]
-                    if i < len(l_saisie)-1:
-                        rue += " "
-            print(rue)
+            if len(liste_adresses) < nb_prop_max and rue.lower() in voie.lower():
+                # le numero existe dans la rue
+                if str(numero) in dico_adresses_rues[voie].keys():
+                    for com in dico_adresses_rues[voie][str(numero)].keys():
+                        if len(liste_adresses) < nb_prop_max :
+                            liste_adresses.append(str(numero)+" "+voie+", "+com)
+                # le numero n'est pas renseigné mais on propose des options
+                elif numero == None :
+                    for num in dico_adresses_rues[voie].keys():
+                        if len(liste_adresses) < nb_prop_max :
+                            for commune in dico_adresses_rues[voie][num].keys():
+                                if len(liste_adresses) < nb_prop_max :
+                                    liste_adresses.append(num+" "+voie+", "+commune)
+        # si la liste est encore vide à ce stade, cela signifie que
+        #   le numero est renseigné mais qu'il n'existe pas dans la rue
+        #   on ignore donc le numéro saisi et on propose des alternatives
+        if len(liste_adresses) == 0:
             for voie in dico_adresses_rues.keys():
                 # la rue existe
-                if len(liste_adresses) < 6 and rue.lower() in voie.lower():
-                    # le numero n'existe pas mais on propose des alternatives
-                    if numero == None : #or str(numero) not in dico_adresses_rues[voie].keys(): # ne marche pas bien
-                        print("le numéro n'existe pas")
-                        for num in dico_adresses_rues[voie].keys():
-                            if len(liste_adresses) < 6 :
-                                for commune in dico_adresses_rues[voie][num].keys():
-                                    if len(liste_adresses) < 6 :
-                                        liste_adresses.append(num+" "+voie+" "+commune)
-                    # le numero et la rue existe
-                    elif str(numero) in dico_adresses_rues[voie].keys():
-                        print(numero)
-                        for com in dico_adresses_rues[voie][str(numero)].keys():
-                            if len(liste_adresses) < 6 :
-                                liste_adresses.append(str(numero)+" "+voie+" "+com)
+                if len(liste_adresses) < nb_prop_max and rue.lower() in voie.lower():
+                    for num in dico_adresses_rues[voie].keys():
+                        if len(liste_adresses) < nb_prop_max :
+                            for commune in dico_adresses_rues[voie][num].keys():
+                                if len(liste_adresses) < nb_prop_max :
+                                    liste_adresses.append(num+" "+voie+", "+commune)
+    #cas ou la commune est saisie en plus de la rue
+    elif commune != None and rue != None:
+        print(f"Commune : {commune} + rue : {rue}")
+        # on cherche une correspondance de la saisie avec une commune de la base
+        com_possibles = []
+        for i in range(len(list(dico_adresses_communes.keys()))):
+            if commune.lower() in list(dico_adresses_communes.keys())[i].lower():
+                com_possibles.append(list(dico_adresses_communes.keys())[i])
+
+        # la commune existe et la rue existe dans cette commune
+        if com_possibles != []:
+            for com in com_possibles:
+                if len(liste_adresses) < nb_prop_max and rue in dico_adresses_communes[com].keys():
+                    # le numero existe dans la rue
+                    if str(numero) in dico_adresses_rues[rue].keys():
+                        liste_adresses.append(str(numero)+" "+rue+", "+com)
+                    # le numero n'est pas renseigné ou n'existe pas dans la rue
+                    #   mais on propose des options alternatives
+                    elif numero == None :
+                        for num in dico_adresses_rues[rue].keys():
+                            if len(liste_adresses) < nb_prop_max :
+                                liste_adresses.append(num+" "+rue+", "+com)
+            # si la liste est encore vide à ce stade, cela signifie que
+            #   le numero est renseigné mais qu'il n'existe pas dans la rue
+            #   on ignore donc le numéro saisi et on propose des alternatives
             if len(liste_adresses) == 0:
-                for voie in dico_adresses_rues.keys():
-                    # la rue existe
-                    if len(liste_adresses) < 6 and rue.lower() in voie.lower():
-                        # le numero n'existe pas mais on propose des alternatives
-                        print("le numéro n'existe pas")
-                        for num in dico_adresses_rues[voie].keys():
-                            if len(liste_adresses) < 6 :
-                                for commune in dico_adresses_rues[voie][num].keys():
-                                    if len(liste_adresses) < 6 :
-                                        liste_adresses.append(num+" "+voie+" "+commune)
-    # cas ou seul le numero est marqué
-    elif numero != None:
-        for num in dico_adresses_num.keys():
-            if len(liste_adresses) < 6 and str(numero).lower() in num.lower():
-                for rue in dico_adresses_num[num].keys():
-                    if len(liste_adresses) < 6 :
-                        for commune in dico_adresses_num[num][rue].keys():
-                            if len(liste_adresses) < 6 :
-                                liste_adresses.append(num+" "+rue+" "+commune)
+                for com in com_possibles:
+                    if len(liste_adresses) < nb_prop_max and rue in dico_adresses_communes[com].keys():
+                        for num in dico_adresses_rues[rue].keys():
+                            if len(liste_adresses) < nb_prop_max :
+                                liste_adresses.append(num+" "+rue+", "+com)
+            # si la liste est toujours vide à ce stade, cela signifie que
+            #   la rue est renseigné mais qu'elle n'existe pas dans la/les commune(s)
+            #   on ignore donc la rue saisie et on propose de prendre le centre de la commune
+            if len(liste_adresses) == 0:
+                for com in com_possibles:
+                    if len(liste_adresses) < nb_prop_max :
+                        liste_adresses.append(com+" centre")
+    print()
     print(liste_adresses)
+    print()
+    print()
     return liste_adresses
 
 def dist_lat_lon_deg(start_lat,start_lon,end_lat,end_lon): 
